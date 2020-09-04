ddbd87499154525a49057aaa666e2798e6f138b9
tarz2929 <saitarunmulley@gmail.com>
2020-09-04 10:29:19 -0600

    stored the swear words in an array

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+//alert("hello there");
+
+/*
+function sendEmail() {
+    Email.send({
+        Host : "smtp.mailtrap.io",
+        Username : "<Mailtrap username>",
+        Password : "<Mailtrap password>",
+        To : 'recipient@example.com',
+        From : "sender@example.com",
+        Subject : "Test email",
+        Body : "<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>"
+    }).then(
+      message => alert(message)
+    );
+    }
+    */
+
+    const swearWords = ["feldercarb", "frack", "skinjob", "vulgacarb"]
